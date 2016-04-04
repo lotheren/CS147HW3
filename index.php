861b5049dd6d181dc2f1c7af4da9c432525b4347
Lotheren <lotheren@gmail.com>
2016-04-03 22:21:48 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Top Images</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="styles/site.css">
+    </head>
+
+<div class="container">
+    <div id="sidebar">
+        <p> here we have sign in </p>
+    </div>
+    <div id="header">
+         <h1>Image Rating</h1>
+    </div>
+    <div id="content">
+        content
+    </div>
+    <div id="footer">
+        footer
+    </div>
+</div>
+<div id="wrapper">
+    
+    <body>
+    
+    
+
+    </div>
+
+    </body>
+</html>
+
