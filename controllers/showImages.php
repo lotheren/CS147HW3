f2a4cfc1a5d68452b2cac45e1bea043af1a4ad6c
Lotheren <lotheren@gmail.com>
2016-04-11 18:01:50 -0700

    fixed a range of stuff

--- controllers/showImages.php
+++ controllers/showImages.php
@@ -4,38 +4,59 @@ session_start();
 define('__ROOT__', "/Applications/MAMP/htdocs/hw3");
 require(__ROOT__ . "/controllers/Config.php");
 require(__ROOT__ . "/controllers/CheckLogin.php");
-require(__ROOT__ . "/controllers/updatedbs.php");
+//require(__ROOT__ . "/controllers/updatedbs.php");
 
 $mysqli = new mysqli($db);
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-$userid = "";
-$userName = "";
-
 
 if (CheckLogin() == TRUE) {
     $userName = $_SESSION['login_user'];
 }
 
-
+$user = $_SESSION['login_user'];
 $dirname = "resources/";
 $images = glob($dirname . "*.jpeg");
-$raiting = 0;
-$userRaiting = 0;
+
+
 
 
 echo $print;
 foreach ($images as $image) {
-$name  = substr($image, 10);
+    $raiting = 0;
+    $userRaiting = 0;
     $uploader = "unknown";
+
+    $name = substr($image, 10);
+    $uploader = "unknown";
+    $dateAdded = "unknown";
+
+    $sql = "SELECT userraiting FROM images WHERE username = '$user' and imagename = '$name'";
+    $result = mysqli_query($db, $sql);
+    while($row = $result->fetch_assoc()) {
+        $userRaiting =  $row["userraiting"];
+    }
+
+    $sql = "SELECT userraiting FROM images WHERE imagename = '$name'";
+    $result = mysqli_query($db, $sql);
+    while($row = $result->fetch_assoc()) {
+        $raiting +=  $row["userraiting"];
+    }
+
+    $sql = "SELECT uploader FROM images WHERE imagename = '$name'";
+    $result = mysqli_query($db, $sql);
+    while($row = $result->fetch_assoc()) {
+        $uploader = $row["uploader"];
+    }
+
 
 
     echo '<table>';
     echo '<tr>';
-                echo '<th>'.$name.'</th>';
-              echo '</tr>';
+    echo '<th>' . $name . '</th>';
+    echo '</tr>';
     echo '<tr>';
     echo '<td><img src="' . $image . '" width="200" height="200"/></td>';
     echo '<tr>';
@@ -45,8 +66,10 @@ $name  = substr($image, 10);
     echo '<tr>';
     echo '<td> Up loader ' . $uploader . ' </td>';
     echo '<tr>';
+    echo '<td> Date Added ' . $dateAdded . ' </td>';
+    echo '<tr>';
     echo '<td>';
-    echo '<form name='.$image.' method="post" action="">';
+    echo '<form name=' . $image . ' method="post" action="">';
     if (CheckLogin() == TRUE) {
         echo '<select required name = "userSelectRaiting" >
   	<option value = "-" > -</option >
@@ -62,11 +85,32 @@ $name  = substr($image, 10);
     }
     echo '</tr>';
 
-}
-echo '</table>';
-if (isset($_POST['userSelectRaiting'])) {
-    $imagename = $_POST['$image'];
-    $userSelectRaiting = $_POST["userSelectRaiting"];
-    imageRaiting($imagename, $userSelectRaiting);
+
+    echo '</table>';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $userSelectRaiting = mysqli_real_escape_string($db, $_POST['userSelectRaiting']);
+
+
+            $sql = "SELECT imagename FROM images WHERE username = '$user' and imagename = '$name'";
+            $result = mysqli_query($db, $sql);
+            $count2 = mysqli_num_rows($result);
+
+            if ($count2 < 1) {
+
+                $sql = "INSERT INTO images (imagename, username,userraiting)
+                VALUES ('$name', '$user', '$userSelectRaiting')";
+                if ($db->query($sql) === TRUE) {
+                    echo "Raiting recorded!";
+
+                }
+                $db->close();
+            }else {echo "Already rated."; }
+
+        } else {
+            $error = "Some error. ";
+        }
+
+
 }
 ?>
--- controllers/updatedbs.php
+++ controllers/updatedbs.php
@@ -1,16 +1,38 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kaigen
- * Date: 4/05/16
- * Time: 9:58 AM
- */
+session_start();
+define('__ROOT__', "/Applications/MAMP/htdocs/hw3");
+require(__ROOT__ . "/controllers/Config.php");
 
 
+$user = $_SESSION['login_user'];
 
-function imageRaiting($image, $raiting){
-    echo $image;
-    echo $raiting;
-
-
+$mysqli = new mysqli($db);
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
 }
+$user = $_SESSION['login_user'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userSelectRaiting = mysqli_real_escape_string($db, $_POST['userSelectRaiting']);
+    $sql = "SELECT userid FROM Users WHERE username = '$user'";
+    $result = mysqli_query($db, $sql);
+    $count = mysqli_num_rows($result);
+    if ($count > 0) {
+
+            $sql = "INSERT INTO images (imagename, userid,userraiting)
+                VALUES ('$name', '$user', '$userSelectRaiting')";
+            if ($db->query($sql) === TRUE) {
+                echo "Raiting recorded!";
+
+
+            } else {
+                echo "Error: " . $sql . "<br>" . $db->error;
+            }
+
+            $db->close();
+
+    } else {
+        $error = "Some error. ";
+    }
+}
+?>
